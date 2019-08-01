1b31badce76c922fc3adc2dff81d42add6df1d7f
elaintran <elain.ttran@gmail.com>
2019-08-01 12:14:52 -0500

    folder testing

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//express parsing middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+// connect to mongo database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// start server; react is listening on 3000, so need to use 3001
+app.listen(PORT, () => console.log(`API Server now listening on PORT ${PORT}!`));
